0b5a0e8d4af4eece481dce67320a8c92b3e0812a
Putra Sangaji Muslim <Putra.Sangaji@VADS.co.id>
2023-05-24 23:12:55 +0700

    first-commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+<nav class="fixed top-0 left-0 w-full z-10 w-full bg-gray-800 py-4 shadow-lg">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex justify-between items-center">
+        <div class="text-white font-bold">Brand Name</div>
+        <div class="lg:flex md:flex sm:flex hidden">
+          <a href="#" class="text-white px-3 py-2 hover:text-gray-400">Menu 1</a>
+          <a href="#" class="text-white px-3 py-2 hover:text-gray-400">Menu 2</a>
+          <a href="#" class="text-white px-3 py-2 hover:text-gray-400">Menu 3</a>
+        </div>
+      </div>
+    </div>
+</nav>
+<!-- <div class="container mx-auto px-4 py-8 pt-20 mx-auto max-w-screen-lg">
+    <h1 class="text-2xl font-bold mb-4">Judul Konten Utama</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae dolor vel tortor lacinia vehicula. Nam congue euismod massa, vitae blandit ipsum vehicula vel. Duis quis felis quis nisi euismod dapibus sed id tellus. Donec euismod neque vitae mauris lacinia, nec semper enim feugiat.</p>
+    <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div class="relative">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/800px-WhatsApp.svg.png" alt="Gambar 1" class="w-full h-full object-cover">
+        </div>
+        <div class="relative">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/800px-WhatsApp.svg.png" alt="Gambar 2" class="w-full h-full object-cover">
+        </div>
+        <div class="relative">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/800px-WhatsApp.svg.png" alt="Gambar 3" class="w-full h-full object-cover">
+        </div>
+        <div class="relative">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/800px-WhatsApp.svg.png" alt="Gambar 4" class="w-full h-full object-cover">
+        </div>
+    </div>
+</div> -->
+
+<!-- <div class="container mx-auto mt-20">
+  <div class="p-4 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+    <div class="bg-gray-200 h-64"></div>
+    <div class="bg-gray-200 h-64"></div>
+    <div class="bg-gray-200 h-64"></div>
+    <div class="bg-gray-200 h-64"></div>
+  </div>
+</div> -->
+
+
+</body>
+</html>
